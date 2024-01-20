84646be62180412c27999678abf2af02c85cdb0c
Sakamura <100300797+Sakamuraa@users.noreply.github.com>
2024-01-20 12:22:54 +0000

    added keepAlive

--- bot.js
+++ bot.js
@@ -42,6 +42,26 @@ process.on("unhandledRejection", (err) => client.logger.error(`Unhandled excepti
     await initializeMongoose();
   }
 
+  // keepAlive
+  const http = require("http");
+
+  const server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.end(`
+    <html>
+      <head>
+        <title>Your Web View</title>
+      </head>
+      <body style="margin: 0; padding: 0;">
+        <iframe width="100%" height="100%" src="https://axocoder.vercel.app/" frameborder="0" allowfullscreen></iframe>
+      </body>
+    </html>`);
+  });
+
+  server.listen(8080, () => {
+    console.log("Server Online because of Skynet System ✅");
+  });
+
   // start the client
   await client.login(process.env.BOT_TOKEN);
 })();
