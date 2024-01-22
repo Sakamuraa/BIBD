74aa1d259836aac5804bd13207c6caa7d91e8ee5
Sakamura <100300797+Sakamuraa@users.noreply.github.com>
2024-01-22 13:04:20 +0000

    remove test guild

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   INTERACTIONS: {
     SLASH: true, // Should the interactions be enabled
     CONTEXT: true, // Should contexts be enabled
-    GLOBAL: false, // Should the interactions be registered globally
-    TEST_GUILD_ID: "1103945195793432617", // Guild ID where the interactions should be registered. [** Test you commands here first **]
+    GLOBAL: true, // Should the interactions be registered globally
+    TEST_GUILD_ID: "", // Guild ID where the interactions should be registered. [** Test you commands here first **]
   },
   EMBED_COLORS: {
     BOT_EMBED: "#46fae2",
